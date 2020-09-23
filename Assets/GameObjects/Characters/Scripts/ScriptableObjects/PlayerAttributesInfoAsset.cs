5a7d3d6141371f4221fde3c77a33842ac1ba83f1
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-24 00:11:38 +0300

    feat(playerAttributesInfoAsset): Add playerAttributesInfoAsset

---
+++ Assets/GameObjects/Characters/Scripts/ScriptableObjects/PlayerAttributesInfoAsset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerAttributesInfoAsset", menuName = "ProjectARPG/Player Attributes Info Asset")]
+public class PlayerAttributesInfoAsset : AttributesInfoAsset
+{
+    [Header("Specific Attributes")]
+    [Range(0, 100)]
+    public float rotateSpeed = 1f;
+    [Range(0, 100)]
+    public float jumpSpeed = 1f;
+}
