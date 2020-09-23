feb02d6e2b617e1e7fc1a04c0e52e456d9793dd3
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-24 00:10:56 +0300

    feat(enemyAttributesInfoAsset): Add enemyAttributesInfoAsset

---
+++ Assets/GameObjects/Characters/Scripts/ScriptableObjects/EnemyAttributesInfoAsset.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyAttributesInfoAsset", menuName = "ProjectARPG/Enemy Attributes Info Asset")]
+public class EnemyAttributesInfoAsset : AttributesInfoAsset
+{
+
+}
