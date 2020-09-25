848847b339fca28a9f300e6a1185be6c4c5bc548
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-25 21:01:14 +0300

    feat(extensions): Add Extensions

---
+++ Assets/Scripts/Utilities/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions
+{
+    public static bool IsNull<T>(this T unityObject) where T : class
+    {
+        return unityObject == null || unityObject.Equals(null);
+    }
+
+    public static T GetScriptableObject<T>(string scriptableObjectFileName) where T : ScriptableObject
+    {
+        T scriptableObject = Resources.Load<T>(scriptableObjectFileName);
+
+        return scriptableObject ? scriptableObject : null;
+    }
+}
