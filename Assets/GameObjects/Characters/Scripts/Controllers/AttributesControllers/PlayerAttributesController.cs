cc7b820e5b8cff5969a41d62a9d8d2540c44f007
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-25 21:00:36 +0300

    feat(controller): Add PlayerAttributesController

---
+++ Assets/GameObjects/Characters/Scripts/Controllers/AttributesControllers/PlayerAttributesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributesController : AttributesController
+{
+    protected PlayerAttributesInfoAsset playerAttributesInfoAsset;
+
+    public float JumpSpeed { get; set; }
+    public float RotateSpeed { get; set; }
+
+    protected virtual void Start()
+    {
+        playerAttributesInfoAsset = (PlayerAttributesInfoAsset)attributesInfoAsset;
+
+        ResetAllAttributes();
+    }
+
+    public override void ResetAllAttributes()
+    {
+        base.ResetAllAttributes();
+
+        JumpSpeed = playerAttributesInfoAsset.rotateSpeed;
+        RotateSpeed = playerAttributesInfoAsset.rotateSpeed;
+    }
+}
