ccf6bb6a0d798976e1101dece4d3d17f2b042abb
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-25 21:08:07 +0300

    feat(controller): Add PlayerController

---
+++ Assets/GameObjects/Characters/Scripts/Controllers/CharacterControllers/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Player
+{
+    CharacterController characterController;
+
+    public PlayerAttributesController PlayerAttributesController { get; private set; }
+    public PlayerAnimationController AnimationController { get; private set; }
+
+    public Vector3 playerVelocity;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        PlayerAttributesController = (PlayerAttributesController) AttributesController;
+        AnimationController = GetComponent<PlayerAnimationController>();
+    }
+
+    void FixedUpdate()
+    {
+        Move(new Vector3(Input.GetAxis("Horizontal"), 0, Mathf.Clamp(Input.GetAxis("Vertical"), -1, .5f)));
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack(Input.GetAxis("Horizontal"));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            EventManager.getInstance().playerEvents.onPlayerPressedShift.Invoke(Input.GetKey(KeyCode.LeftShift));
+        }
+
+        CalculateVerticalPosition();
+
+        Turn();
+    }
+
+    private void CalculateVerticalPosition()
+    {
+        if (!characterController.isGrounded)
+        {
+            playerVelocity.y += PlayerAttributesController.Gravity * Time.deltaTime;
+        }
+
+        PlayerAttributesController.JumpSpeed = AnimationController.GetFloat(PlayerAnimationController.JUMP_SPEED);
+        playerVelocity.y = PlayerAttributesController.JumpSpeed * Time.deltaTime;
+
+        characterController.Move(playerVelocity);
+    }
+
+    private void LateUpdate()
+    {
+
+    }
+
+    void Attack(float movementHorizontal)
+    {
+        EventManager.getInstance().playerEvents.onPlayerAttack.Invoke(movementHorizontal);
+    }
+
+    void Jump()
+    {
+        EventManager.getInstance().playerEvents.onPlayerJump.Invoke();
+    }
+
+    void Move(Vector3 direction)
+    {
+        EventManager.getInstance().playerEvents.onPlayerMove.Invoke(direction);
+    }
+
+    void Turn()
+    {
+        float mouseAccX = Mathf.Clamp(Input.GetAxis("Mouse X"), -1f, 1f);
+        float rotationOnY = PlayerAttributesController.RotateSpeed * mouseAccX;
+
+        transform.Rotate(0, rotationOnY, 0);
+
+        float animatorRotAccValue = AnimationController.GetFloat(PlayerAnimationController.ROTATION_ACC);
+        EventManager.getInstance().playerEvents.onPlayerTurn.Invoke(Mathf.Lerp(animatorRotAccValue, mouseAccX, 0.1f));
+    }
+
+    #region Damage
+
+    public override void ApplyDamage(float damage, DamageType damageType)
+    {
+
+    }
+
+    #endregion
+}
