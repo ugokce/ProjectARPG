0bfcca4b8bed4acf993b39c640867ee06a44207b
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-28 01:09:28 +0300

    feat(contactController): Add ContactController

---
+++ Assets/Scripts/Controllers/DamageControllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactController : CustomMonoBehaviour
+{
+    public Entity Owner { get; private set; }
+
+    void Awake()
+    {
+        Owner = GetComponentInParent<Entity>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var targetEntity = other.GetComponentInParent<Entity>();
+
+        if (targetEntity.IsNull())
+        {
+            return;
+        }
+
+        if (targetEntity.AttributesController.TargetType != Owner.AttributesController.TargetType)
+        {
+            var damageController = targetEntity.gameObject.GetComponent<DamageController>();
+
+            if(damageController.IsNull())
+            {
+                return;
+            }
+
+            damageController.AddDamage(Owner.AttributesController.ContactDamageInfo, targetEntity.gameObject);
+        }
+    }
+}
