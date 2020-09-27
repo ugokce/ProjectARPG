0c54c8eaea670c96b6f11916f498dd2dd61826a4
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-28 00:59:09 +0300

    feat(IAttributesInfoAsset): Add IAttributesInfoAsset

---
+++ Assets/Scripts/Interfaces/IAttributesInfoAsset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAttributesInfoAsset
+{
+    EntityType EntityType { get; }
+
+    int MinAttributeValue { get; }
+    int MaxAttributeValue { get; }
+}
