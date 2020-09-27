771a4149411061f37afdd1270c370c9c8d2c9932
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-28 01:02:30 +0300

    feat(RangedIntValue): Add RangedIntValue

---
+++ Assets/Scripts/Controllers/DamageControllers/RangedIntValue.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RangedIntValue
+{
+    public int Value
+    {
+        get
+        {
+            return Mathf.Clamp(baseValue + BonusTempValue.Result, minValue, maxValue);
+        }
+
+        set
+        {
+            OldValue = Value;
+            baseValue = Mathf.Clamp(value, minValue, maxValue);
+        }
+    }
+
+    public int OldValue { get; private set; }
+    public int DeltaValue => Value - OldValue;
+
+    public BonusValue<int> BonusTempValue { get; } = new BonusValue<int>(BonusValue<int>.CalculateAdditiveInt);
+
+    int intialBaseValue;
+    int baseValue;
+    int minValue;
+    int maxValue;
+
+    public RangedIntValue(int baseValue, int minValue, int maxValue)
+    {
+        this.baseValue = baseValue;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+
+        OldValue = baseValue;
+        intialBaseValue = baseValue;
+    }
+
+    public void Reset()
+    {
+        baseValue = intialBaseValue;
+        BonusTempValue.Clear();
+    }
+
+    public static implicit operator int(RangedIntValue rangeIntValue)
+    {
+        return rangeIntValue.Value;
+    }
+}
