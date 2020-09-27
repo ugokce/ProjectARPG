3ef3a0c47c1c479ec79aaa6385151125e5e93d11
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-28 01:08:04 +0300

    feat(isDamageable): Add IsDamageable feat(applyDamage): Add ApplyDamage feat(addStatus): Add AddStatus feat(removeStatus): Add RemoveStatus feat(die): Add Die

--- Assets/GameObjects/Characters/Scripts/Entities/Entity.cs
+++ Assets/GameObjects/Characters/Scripts/Entities/Entity.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(DamageController))]
 public class Entity : CustomMonoBehaviour, IDamageable
 {
     public AttributesController AttributesController { get; protected set; }
 
     public static List<Entity> Entities { get; set; } = new List<Entity>();
 
+    public bool IsDamageable { get; protected set; } = true;
     public bool IsFollowable() => isFollowable;
 
     bool isFollowable = true;
@@ -27,7 +29,27 @@ public class Entity : CustomMonoBehaviour, IDamageable
         Entities.Remove(this);
     }
 
-    public virtual void ApplyDamage(float damage, DamageType damageType)
+    #region IDamage
+
+    public virtual void ApplyDamage(IList<AppliedDamageInfo> appliedDamageInfos, DamageType damageType, GameObject damageSource = null)
     {
+        
+    }
+
+    public virtual void AddStatus(DamageStatusType damageStatusType)
+    {
+        
+    }
+
+    public virtual void RemoveStatus(DamageStatusType damageStatusType)
+    {
+        
+    } 
+
+    #endregion
+
+    public virtual void Die()
+    {
+        
     }
 }
