ecc6a95d7ca6fcfcadcacbb60b0c1654e50b214f
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-28 01:09:10 +0300

    feat(objectPool): Add ObjectPool

---
+++ Assets/Scripts/ObjectPool/ObjectPool.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T> where T : new()
+{
+    List<T> pool = new List<T>();
+
+    public T Pull()
+    {
+        if (pool.Count < 1)
+            return new T();
+
+        int lastIndex = pool.Count - 1;
+        T pooledObject = pool[lastIndex];
+        pool.RemoveAt(lastIndex);
+
+        return pooledObject;
+    }
+
+    public void Push(T pooledObject)
+    {
+        pool.Add(pooledObject);
+    }
+
+    public void Push(IList<T> pooledObjects)
+    {
+        pool.AddRange(pooledObjects);
+    }
+}
