00280bad49ac31c8b63cd1751631a0af2f059344
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-28 01:03:01 +0300

    feat(RangedFloatValue): Add RangedFloatValue

---
+++ Assets/Scripts/Controllers/DamageControllers/RangedFloatValue.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedFloatValue
+{
+    public float Value
+    {
+        get
+        {
+            return Mathf.Clamp(baseValue + BonusTempValue.Result, minValue, maxValue);
+        }
+
+        set
+        {
+            OldValue = Value;
+            baseValue = Mathf.Clamp(value, minValue, maxValue);
+        }
+    }
+
+    public float OldValue { get; private set; }
+    public float DeltaValue => Value - OldValue;
+
+    public BonusValue<float> BonusTempValue { get; } = new BonusValue<float>(BonusValue<float>.CalculateAdditiveFloat);
+
+    float intialBaseValue;
+    float baseValue;
+    float minValue;
+    float maxValue;
+
+    public RangedFloatValue(float baseValue, float minValue, float maxValue)
+    {
+        this.baseValue = baseValue;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+
+        OldValue = baseValue;
+        intialBaseValue = baseValue;
+    }
+
+    public void Reset()
+    {
+        baseValue = intialBaseValue;
+        BonusTempValue.Clear();
+    }
+
+    public static implicit operator float(RangedFloatValue rangeFloatValue)
+    {
+        return rangeFloatValue.Value;
+    }
+}
