1896d75f9d09ed6ba5e12833f5fb75a531ece666
Melih Kazım PAMPAL <melihkazim01@gmail.com>
2020-09-28 01:17:03 +0300

    feat(script): Add statusEffectInfoAsset feat(statusEffectInfoAsset): Add statusEffectInfoAsset

---
+++ Assets/GameObjects/Characters/Scripts/Status/StatusEffectInfoAsset.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StatusEffectInfoAsset", menuName = "ProjectARPG/Status Effects/Status Effect Info Asset")]
+public class StatusEffectInfoAsset : ScriptableObject
+{
+    [Space]
+    public Dictionary<DamageStatusType, GameObject> statusEffectInfoDict;
+}
